0d5709a507e91960c91029221f241447e1a8a6ae
masafumi <masafumi@example.com>
2020-06-20 00:42:09 +0900

    ProviderパターンModel

--- lib/main.dart
+++ lib/main.dart
@@ -1,85 +1,47 @@
-import 'package:coriander/next_page.dart';
+import 'package:coriander/main_model.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('コリアンダー'),
-        ),
-        body: Center(
-          child: Column(
-            children: <Widget>[
-              Text(
-                'KBOYさんじゃないよ！',
-              )
-            ],
+      home: ChangeNotifierProvider<MainModel>(
+        create: (_) => MainModel(),
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text('コリアンダー'),
+          ),
+          body: Consumer<MainModel>(builder: (context, model, child) {
+              return Center(
+                child: Column(
+                  children: [
+                    Text(
+                      model.kboyText,
+                      style: TextStyle(
+                        fontSize: 30,
+                      ),
+                    ),
+                    RaisedButton(
+                      child: Text('ボタン'),
+                      onPressed: (){
+                        //ここで何かする
+                        model.changeKboyText();
+                      },
+                    ),
+                  ],
+                ),
+              );
+            }
           ),
         ),
       )
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  final items = List<String>.generate(5, (i) => "Item $i");
-
-  @override
-  Widget build(BuildContext context) {
-      return Scaffold(
-        appBar: AppBar(
-          title: Text('KBOYのFlutter大学'),
-        ),
-      body: Container(
-        width: double.infinity,
-        child: ListView(
-          // This next line does the trick.
-          scrollDirection: Axis.vertical,
-          children: <Widget>[
-            Container(
-              width: 160.0,
-              height: 20,
-              color: Colors.red,
-            ),
-            Container(
-              width: 160.0,
-              height: 20,
-              color: Colors.blue,
-            ),
-            Container(
-              width: 160.0,
-              height: 20,
-              color: Colors.green,
-            ),
-            Container(
-              width: 160.0,
-              height: 20,
-              color: Colors.yellow,
-            ),
-            Container(
-              width: 160.0,
-              color: Colors.orange,
-            ),
-          ],
-        )
-      ),
     );
   }
 }
